ad73c1098082501e382882c09c6b45b1131e0c59
Daniel Nuno <daniel.nunojeda@gmail.com>
2025-05-13 02:09:19 -0600

    Agregar Google Analytics 4 y Meta Pixel en layout.js; actualizar número de WhatsApp

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import "leaflet/dist/leaflet.css";
 import "../styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 import ClientWhatsAppButton from '../components/ClientWhatsAppButton';
 import Link from 'next/link';
 
@@ -40,6 +41,52 @@ export default function RootLayout({ children }) {
         />
       </Head>
       <body className="flex flex-col min-h-screen pt-16">
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DZCY3C3DEJ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DZCY3C3DEJ');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel Script */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '9719096518211708');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=9719096518211708&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         {/* Contenido principal */}
         <main className="flex-grow">{children}</main>
 
